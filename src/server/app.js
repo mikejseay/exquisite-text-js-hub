9139f6857d1d5bd11c5df666c30066070ba7cea1
Mike Seay <mikejseay@gmail.com>
2022-06-22 17:37:40 -0700

    Use dotenv in the server better

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,8 @@
 // I'm guessing this kind of puts everything together?
 
-const herokuDeploy = false;
-
 // CORS stands for Cross-Origin Resource Sharing
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 var cors = require('cors');
-
-
 var express = require('express');               // the app framework library
 var path = require('path');                     // for joining paths
 var cookieParser = require('cookie-parser');    // for parsing cookies
@@ -23,12 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-if (herokuDeploy) {
-    app.use(express.static(path.join(__dirname, '../../build')));
-} else {
-    app.use(express.static(path.join(__dirname, 'public')));
-}
+const isProduction = process.env.NODE_ENV === "production";
+const targetDir = isProduction ? '../../build' : 'public';
 
+app.use(express.static(path.join(__dirname, targetDir)));
 app.use('/', indexRouter);                      // sets the router for the root page /
 app.use('/users', usersRouter);                 // sets the router for the /users page
 
